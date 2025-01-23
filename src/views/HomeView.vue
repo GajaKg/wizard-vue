2c723ee63dd5dcb72608fbcca41e0bbd3db08ad2
Milan Gajic <boromirsrb@yahoo.com>
2025-01-23 22:21:37 +0100

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// var Hand = require('pokersolver').Hand;
+import { Hand } from "pokersolver";
+var hand1 = Hand.solve(["Ad", "As", "Jc", "Th", "2d", "3c", "Kd"]);
+var hand2 = Hand.solve(["Ad", "As", "Jc", "Th", "2d", "Qs", "Qd"]);
+var winner = Hand.winners([hand1, hand2]); // hand2
+
+console.log(winner);
+</script>
+
+<template>
+  <main>home</main>
+</template>
